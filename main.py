d02abb75295257096322209395addf30e5898adb
klol <adg08101@gmail.com>
2021-04-21 17:45:41 -0400

    Initial commit

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ def main():
     source_file = open('source.txt', mode='r', encoding='utf-8')
     txt = source_file.read()
 
+    l = []
+
     for _ in range(76):
-        print(txt[int(txt.find(start)) + len(start):int(txt.find(start)) + int(txt[int(txt.find(start)):].find(end))])
+        line = txt[int(txt.find(start)) + len(start):
+                   int(txt.find(start)) + int(txt[int(txt.find(start)):].find(end))].replace('\n', '')
+        print(line)
         txt = txt[int(txt.find(start)) + int(txt[int(txt.find(start)):].find(end)) + len(end):]
+        l.append(line + '\n')
+
+    source_file.close()
+
+    f = open("result.txt", "w")
+
+    for i in l:
+        f.writelines(i)
+
+    f.close()
 
 
 if __name__ == '__main__':
